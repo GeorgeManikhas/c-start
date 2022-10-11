8347bc8f9b7a7b6ba05403199ee7ab9496743299
GeorgeM <gemanikhas@mail.ru>
2022-10-11 20:30:01 +0300

    intetesting02

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Ведите два числа и я скажу вам сумму");
-int numberA = Console.Readline();
-int numberB = Console.Readline();
+string numberA = Console.Readline();
+string numberB = Console.Readline();
 int result = numberA + numberB;
 Console.Writeline(result);
 
