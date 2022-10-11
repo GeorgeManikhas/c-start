f2ace5d2d80c07988079871a151b1ada49489006
GeorgeM <gemanikhas@mail.ru>
2022-10-11 21:16:37 +0300

    example005ifelseToLower

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Как тебя зовут?");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, наконец-то ты пришла!");
+} 
+else
+{
+    Console.Write("Здравствуйте! ");
+    Console.WriteLine(username);
+}
+
